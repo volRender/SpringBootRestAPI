aed3e2facd6981b827440c8d8f3dadafd6003ef0
volRender <litvishko.render@gmail.com>
2021-12-29 15:13:51 +0300

    Create new project.

---
+++ src/main/java/com/litvishko/spring/boot/rest_api/spring_boot_rest_api/SpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.litvishko.spring.boot.rest_api.spring_boot_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/litvishko/spring/boot/rest_api/spring_boot_rest_api/SpringBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litvishko.spring.boot.rest_api.spring_boot_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
